e8da11606c80b1c836839d43f1d18c5dbe8179f6
Lochmannweb <lochmannweb@gmail.com>
2024-08-08 15:14:45 +0200

    supabase

--- src/app/lib/supabaseClient.js
+++ src/app/lib/supabaseClient.js
@@ -1,14 +1,7 @@
-// lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseAnonKey) {
-    console.error('Supabase URL or ANON key is missing.');
-    // Handle the error gracefully
-    throw new Error('Supabase URL or ANON key is missing.');
-}
 
-// Only create the client if the environment variables are present
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
